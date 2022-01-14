326dcc639b20205f4268a1c1145faaa157675c83
69hunter <kltan69@gmail.com>
2022-01-14 17:19:58 +0800

    fix: fix naming of configuration

--- src/config/smart-contract.config.ts
+++ src/config/smart-contract.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 
 export const smartContractConfig = registerAs('smartContract', () => ({
   network: process.env.SMART_CONTRACT_NETWORK,
-  infuraProjectId: process.env.INFURA_PROJECT_ID,
-  infuraProjectSecret: process.env.INFURA_PROJECT_SECRET,
+  infuraProjectId: process.env.SMART_CONTRACT_INFURA_PROJECT_ID,
+  infuraProjectSecret: process.env.SMART_CONTRACT_INFURA_PROJECT_SECRET,
   erc721Address: process.env.SMART_CONTRACT_ERC721_ADDRESS,
 }));
